3ae9e446df2f7ec5199dc9bb779a609e48e6594f
arshajworkdesk <arshajwork@gmail.com>
2025-11-19 19:52:57 +0530

    .env added for prod

--- auth-service/src/main/java/com/ukclub/auth/constants/JwtConstants.java
+++ auth-service/src/main/java/com/ukclub/auth/constants/JwtConstants.java
@@ -13,8 +13,6 @@ public final class JwtConstants {
     public static final String CLAIM_USER_ID = "userId";
     public static final String CLAIM_ROLE = "role";
 
-    // Default Configuration Values
-    public static final String DEFAULT_SECRET = "your-256-bit-secret-key-change-this-in-production-minimum-32-characters";
-    public static final long DEFAULT_EXPIRATION_MS = 3600000L; // 1 hour in milliseconds
+
 }
 
